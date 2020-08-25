f939fb34e5cc75958baa9e667b36bd289c2842b8
anback <anders.back@me.com>
2020-08-25 08:25:50 +0200

    Update DragDropMaterialTable.js

--- src/DragDropMaterialTable.js
+++ src/DragDropMaterialTable.js
@@ -4,50 +4,43 @@ import MTableBodyRow from 'material-table/dist/components/m-table-body-row'
 
 const doNothing = () => {}
 export default (props) => {
-  let {onDragEnd, onDragEnter, sortId} = props
+  let {onDragEnd} = props
   const [data, setData] = useState(props.data)
-  console.log(data)
   onDragEnd = onDragEnd || doNothing
-  onDragEnter = onDragEnter || doNothing
-  const DragState = { row: -1, dropIndex: -1  };
+  const DragState = { index: -1, dropIndex: -1  };
 
   return (
     <MaterialTable
           {...props}
           data={data}
-            options={{
-                rowStyle: (data, index) => {
-                    console.log('rowStyle')
-                    return {backgroundColor: index % 2 === 0 ? 'grey' : 'white'}
-                }
-            }}
           components={{
               Row: props => (
                   <MTableBodyRow {...props}
-                  draggable="true"
-                  onDragStart={(e) => {DragState.index = props.data.tableData.id}}
+                    draggable="true"
+                    onDragStart={(e) => {DragState.index = props.data.tableData.id}}
+                    onDragEnter={e => {
+                        e.preventDefault();
+                        if( props.data.tableData.id !== DragState.index){
+                            DragState.dropIndex = props.data.tableData.id;
+                        }                                                  
+                    }}
 
-                  onDragEnter={e => {
-                      e.preventDefault();
-                      if( props.data.tableData.id !== DragState.index){
-                          DragState.dropIndex = props.data.tableData.id;
-                          onDragEnter(DragState)
-                          setData([...data.reverse().map(item => ({...item, tableData: undefined}))])
-                      }                                                  
-                  }}
-
-                  onDragEnd={(e) => {
-                      console.log('onDragEnd', DragState)
-                      if(DragState.dropIndex !== -1) {
-                        console.log('setData')
-                        setData([...data.reverse().map(item => ({...item, tableData: undefined}))])
-                        onDragEnd(DragState)
-                      }
-                      DragState.index = -1;
-                      DragState.dropIndex = -1;
-                  }}
+                    onDragEnd={(e) => {
+                        e.preventDefault();
+                        if(DragState.dropIndex !== -1) {
+                            const newData = data.reduce((a,b, i) => {
+                                if(i === DragState.index) {a.push({...data[DragState.dropIndex], tableData: undefined}); return a}
+                                if(i === DragState.dropIndex) {a.push({...data[DragState.index], tableData: undefined}); return a}
+                                a.push({...b, tableData: undefined})
+                                return a
+                            }, [])
+                            setData(newData)
+                            onDragEnd(newData)
+                        }
+                        DragState.index = -1;
+                        DragState.dropIndex = -1;
+                    }}
                   />
-
               )
           }}
       />
