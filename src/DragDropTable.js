887124e6bebe9bb499dce6f97e2d60541b38dcdb
anback <anders.back@me.com>
2020-08-28 13:26:45 +0200

    sdf

--- src/DragDropTable.js
+++ src/DragDropTable.js
@@ -1,13 +1,5 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-import Paper from '@material-ui/core/Paper';
-import { DroppableComponent as getDroppableComponent, DraggableComponent as getDraggableComponent} from './Common';
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 
 export default ({columns, data}) => {
   const DroppableComponent = getDroppableComponent(console.log)
@@ -35,3 +27,53 @@ const styles = {
     margin : 10
   }
 }
+
+export const getDroppableComponent = (onDragEnd: (result: DropResult, provided: ResponderProvided) => void) => 
+  (props: any) => 
+    <DragDropContext onDragEnd={onDragEnd}>
+      <Droppable droppableId={'droppableId'}>
+          {(provided) => {
+              return (
+                  <div ref={provided.innerRef} {...provided.droppableProps} {...props}>
+                      {props.children}
+                      {provided.placeholder}
+                  </div>
+              );
+          }}
+      </Droppable>
+    </DragDropContext>
+
+export const getDraggableComponent = (id: string, index: number) => (props: any) => {
+
+    return (
+        <Draggable draggableId={id} index={index}>
+            {(provided, snapshot) => {
+                return <div
+                ref={provided.innerRef}
+                {...provided.draggableProps}
+                {...provided.dragHandleProps}
+                {...props}
+                style={getItemStyle(
+                    snapshot.isDragging,
+                    provided.draggableProps.style
+                  )}>
+                {props.children}
+                
+            </div>
+            }}
+        </Draggable>
+    );
+};
+
+const getItemStyle = (isDragging, draggableStyle) => ({
+    // some basic styles to make the items look a bit nicer
+    userSelect: "none",
+    padding: 1,
+    margin: `0 0 ${0}px 0`,
+  
+    // change background colour if dragging
+    background: isDragging ? "lightgrey" : "white",
+  
+    // styles we need to apply on draggables
+    ...draggableStyle
+  });
